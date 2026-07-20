5bcc004f51044700334bd032ce3d687a77c0046b
Dangiar21 <tobias.weiss06@gmail.com>
2026-07-20 20:40:39 +0200

    Add install_dependencies.bat script

---
+++ install_dependencies.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installiere benoetigte Python-Bibliotheken fuer das Buchhaltungsprogramm...
+echo.
+
+pip install pandas openpyxl customtkinter reportlab asn1crypto tkinterdnd2
+
+echo.
+echo Installation abgeschlossen! Du kannst das Fenster nun schliessen.
+pause
