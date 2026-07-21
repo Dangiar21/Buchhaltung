3b3fcd3e0de0b04004b4a292a2dac6a9ad728682
Dangiar21 <tobias.weiss06@gmail.com>
2026-07-21 02:37:01 +0200

    Update install_dependencies.bat with google-genai

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -2,7 +2,7 @@
 echo Installiere benoetigte Python-Bibliotheken fuer das Buchhaltungsprogramm...
 echo.
 
-pip install pandas openpyxl customtkinter reportlab asn1crypto tkinterdnd2
+pip install pandas openpyxl customtkinter reportlab asn1crypto tkinterdnd2 google-genai
 
 echo.
 echo Installation abgeschlossen! Du kannst das Fenster nun schliessen.
