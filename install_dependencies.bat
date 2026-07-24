f374e150e3f62e3b017c21f6458702cb0cb9955f
Dangiar21 <tobias.weiss06@gmail.com>
2026-07-24 18:45:35 +0200

    Nutze requirements.txt in install script

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Installiere benoetigte Python-Bibliotheken fuer das Buchhaltungsprogramm...
 echo.
-pip install pandas openpyxl customtkinter reportlab asn1crypto tkinterdnd2 defusedxml python-dotenv google-genai sqlalchemy pydantic
+pip install -r requirements.txt
 echo.
 echo Installation abgeschlossen! Du kannst das Fenster nun schliessen.
 pause
